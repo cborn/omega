b3a9955ee6f61882f40d0052bfc1df385441754f
Lee Williams <lw@reddragonsoftworks.com>
2019-09-13 15:52:31 +0100

    added in grading ability.

--- client/src/app/faculty/answer-grading-renderer/answer-grading-renderer.component.ts
+++ client/src/app/faculty/answer-grading-renderer/answer-grading-renderer.component.ts
@@ -1,38 +1,70 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {SubmissionResponse} from '../../Model/submissionResponse';
 import {Question, QuestionType} from '../../Model/question';
+import {CommentAddingDialogComponent} from '../../dialogs/comment-adding-dialog';
+import {MatDialog} from '@angular/material';
 
 @Component({
-  selector: 'app-answer-grading-renderer',
-  templateUrl: './answer-grading-renderer.component.html',
-  styleUrls: ['./answer-grading-renderer.component.css']
+    selector: 'app-answer-grading-renderer',
+    templateUrl: './answer-grading-renderer.component.html',
+    styleUrls: ['./answer-grading-renderer.component.css']
 })
 export class AnswerGradingRendererComponent implements OnInit {
 
 
-  @Input() response: SubmissionResponse;
-  @Input() question: Question;
+    @Input() response: SubmissionResponse;
+    @Input() question: Question;
 
-  constructor() { }
+    constructor( private dialog: MatDialog) {
+    }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+    }
 
-
-  isBordered() {
-    return this.question.type === 0 || this.question.type === 4 ||  this.question.type === 5 ||  this.question.type === 9;
-  }
-
-  isCloze() {
-    return this.question.type === QuestionType.CLOZE;
-  }
+    gradeChanged(event) {
+        this.response.grade = Number(event.target.value);
+    }
 
 
-  isAudio() {
-    return this.question.type === QuestionType.VOICE;
-  }
+    isBordered() {
+        return this.question.type === QuestionType.MULTI_CHOICE || this.question.type === QuestionType.DROPDOWN || this.question.type === QuestionType.BOOLEAN;
+    }
 
-  isPicture() {
-    return this.question.type === QuestionType.PICTURE_CHOICE;
-  }
+    isPlainText() {
+        return this.question.type === QuestionType.NUMBER || this.question.type === QuestionType.DATE || this.question.type === QuestionType.SHORT_TEXT || this.question.type === QuestionType.LONG_TEXT;
+    }
+
+    isCloze() {
+        return this.question.type === QuestionType.CLOZE;
+    }
+
+    isBoolean() {
+        return this.question.type === QuestionType.BOOLEAN;
+    }
+
+    isAudio() {
+        return this.question.type === QuestionType.VOICE;
+    }
+
+    isPicture() {
+        return this.question.type === QuestionType.PICTURE_CHOICE;
+    }
+
+
+    addComment() {
+        const dialogRef = this.dialog.open(CommentAddingDialogComponent, {
+            width: '250px',
+            data: {
+                textOnly: true,
+                response: this.response,
+
+            }
+        });
+
+        dialogRef.afterClosed().subscribe(result => {
+           this.response = result;
+        });
+
+    }
+
 }
--- client/src/app/pipes/position-order.pipe.ts
+++ client/src/app/pipes/position-order.pipe.ts
@@ -1,18 +1,32 @@
-import { Pipe, PipeTransform } from '@angular/core';
+import {Pipe, PipeTransform} from '@angular/core';
 import {Question} from '../Model/question';
+import {SubmissionResponse} from '../Model/submissionResponse';
 
 @Pipe({
-  name: 'positionOrder'
+    name: 'positionOrder'
 })
 export class PositionOrderPipe implements PipeTransform {
 
-  transform(questions: Question[] = [], order: string): Question[] {
-    return questions.sort((prevQuestion: Question, nextQuestion: Question) => {
-      if (order === 'desc') {
-        return nextQuestion.position - prevQuestion.position;
-      } else {
-        return prevQuestion.position - nextQuestion.position;
-      }
-    });
-  }
+    transform(items: any[] = [], order: string): any[] {
+
+        if (items[0] != null && items[0].hasOwnProperty('question')) {
+            return items.sort((prevSubmission: SubmissionResponse, nextSubmission: SubmissionResponse) => {
+                if (order === 'desc') {
+                    return nextSubmission.question.position - prevSubmission.question.position;
+                } else {
+                    return prevSubmission.question.position - nextSubmission.question.position;
+                }
+            });
+        } else {
+            return items.sort((prevQuestion: Question, nextQuestion: Question) => {
+                if (order === 'desc') {
+                    return nextQuestion.position - prevQuestion.position;
+                } else {
+                    return prevQuestion.position - nextQuestion.position;
+                }
+            });
+        }
+
+
+    }
 }
