dda981130211fb8700b6d546cb05a413c181ac9f
Lee Williams <lee.williams@lifefyle.com>
2019-09-16 20:13:56 +0100

    minor style changes

--- server/grails-app/conf/spring/resources.groovy
+++ server/grails-app/conf/spring/resources.groovy
@@ -1,7 +1,5 @@
 import omega.UserPasswordEncoderListener
-import omega.UserPasswordEncoderListener
 // Place your Spring DSL code here
 beans = {
-    userPasswordEncoderListener(UserPasswordEncoderListener)
     userPasswordEncoderListener(UserPasswordEncoderListener)
 }
