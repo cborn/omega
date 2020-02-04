a84c07c64ef112e613f1d55877946855311cb92c
Lee Williams <lw@reddragonsoftworks.com>
2020-02-04 15:50:37 +0000

    fixing issue

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -46,7 +46,7 @@ export class NavComponent implements OnInit {
     async loadData() {
 
         this.notificationService.reloadRequiredObserver.subscribe(() => {
-            if (this.applicationData.isSuperAdmin && this.sessionService.adminSite == null) {
+            if (this.applicationData != null && this.applicationData.isSuperAdmin && this.sessionService.adminSite == null) {
                 this.applicationData = null;
             }
         });
